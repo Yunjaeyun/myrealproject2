ea0817f444b769f84b945e109825a8bd4adcb107
Yun <jy8767@naver.com>
2023-03-03 19:05:13 +0900

    Initial commit

---
+++ src/main/java/com/example/myrealproject/MyrealprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.myrealproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrealprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrealprojectApplication.class, args);
+	}
+
+}
