0c50067338a8896d148212a1737cf56074f52783
Yun <jy8767@naver.com>
2023-03-03 20:29:33 +0900

    layout 구성

--- src/main/java/com/example/myrealproject/Controller/HomeController.java
+++ src/main/java/com/example/myrealproject/Controller/HomeController.java
@@ -1,2 +1,13 @@
-package main.java.com.example.myrealproject.Controller;public class HomeController {
+package com.example.myrealproject.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String index(){
+        return "index";
+    }
 }
